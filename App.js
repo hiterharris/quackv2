bbe08e7f8480fe7c8b72dbab4591b25ccfe4ff88
Henry Harris IV <henryharrisiv@Henrys-MacBook-Pro-2.local>
2019-06-13 10:25:47 -0400

    set up App.js

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello world!!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
