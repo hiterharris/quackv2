e3dd3690b093955e15efc8db6ac424387371fcd3
Henry Harris IV <henryharrisiv@Henrys-MacBook-Pro-2.local>
2019-06-13 21:31:37 -0400

    added index.js with yelp-fusion api call

---
+++ src/assets/data/index.js
@@ -0,0 +1,17 @@
+const yelp = require('yelp-fusion');
+const apiKey = '4TwtB1xSvyHl5nDWqmOPj_3cHANyKsn8XhO2lBR2xdjRWs52PivbW-wdvQ92uWNIYR76VeQxXfSyh7jREVLe_HBd31tuPk08L5lIsHyEb449yLFbeGnPzbZGDaz_XHYx';
+const searchRequest = {
+    location: 'charlotte'
+};
+const client = yelp.client(apiKey);
+
+client.search(searchRequest).then(response => {
+    const singleResult = response.jsonBody.businesses[0];
+    const results = response.jsonBody.businesses;
+    console.log(JSON.stringify(results, null, 4));
+}).catch(e => {
+    console.log(e);
+});
+
+// cd src/assets/data
+// node index.js
