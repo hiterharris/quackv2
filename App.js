fe41750944ba0dc672459ff93b169a32b8792c24
Henry Harris IV <henryharrisiv@Henrys-MacBook-Pro-2.local>
2019-06-17 21:06:47 -0400

    refactored components

--- App.js
+++ App.js
@@ -3,63 +3,24 @@ import {
   StyleSheet,
   Text,
   View,
-  SafeAreaView,
   Dimensions,
 } from 'react-native';
-import Card from './src/components/Card';
-import Header from './src/components/Header';
-import profiles from './src/assets/data/profiles.json';
-
-const { width, height } = Dimensions.get('window');
+import Swiper from './src/components/Swiper';
 
 export default class App extends Component {
-
-  state = {
-    profileIndex: 0,
-  }
-
-  nextCard = () => {
-    this.setState({ profileIndex: this.state.profileIndex + 1 })
-  }
-
   render() {
-    const { profileIndex } = this.state;
     return (
-      <SafeAreaView style={styles.safeArea}>
-        <View style={styles.appContainer}>
-          <View style={styles.headerWrapper}>
-            <Header />
-          </View>
-          <View style={styles.profileWrapper}>
-            {profiles.slice(profileIndex, profileIndex + 5).reverse().map((profile, i) => {
-              return (
-                <Card
-                  profile={profile}
-                  key={profile.id}
-                  onSwipeOff={this.nextCard}
-                />
-              );
-            })}
-          </View>
-        </View>
-      </SafeAreaView>
+      <View style={styles.appContainer}>
+        <Swiper />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-  },
   appContainer: {
     flex: 1,
     backgroundColor: 'white',
     fontFamily: 'Avenir',
-  },
-  headerWrapper: {
-    alignItems: 'center',
-  },
-  profileWrapper: {
-    flex: 1,
-  },
+  }
 });
---
+++ src/components/Swiper.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    Dimensions,
+} from 'react-native';
+import Card from './Card';
+import Header from './Header';
+import profiles from '../assets/data/profiles.json';
+
+const { width, height } = Dimensions.get('window');
+
+export default class Swiper extends Component {
+
+    state = {
+        profileIndex: 0,
+    }
+
+    nextCard = () => {
+        this.setState({ profileIndex: this.state.profileIndex + 1 })
+    }
+
+    render() {
+        const { profileIndex } = this.state;
+        return (
+            <SafeAreaView style={styles.safeArea}>
+                <View style={styles.appContainer}>
+                    <View style={styles.headerWrapper}>
+                        <Header />
+                    </View>
+                    <View style={styles.profileWrapper}>
+                        {profiles.slice(profileIndex, profileIndex + 5).reverse().map((profile, i) => {
+                            return (
+                                <Card
+                                    profile={profile}
+                                    key={profile.id}
+                                    onSwipeOff={this.nextCard}
+                                />
+                            );
+                        })}
+                    </View>
+                </View>
+            </SafeAreaView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+    },
+    appContainer: {
+        flex: 1,
+        backgroundColor: 'white',
+        fontFamily: 'Avenir',
+    },
+    headerWrapper: {
+        alignItems: 'center',
+    },
+    profileWrapper: {
+        flex: 1,
+    },
+});
