6cc45a6a37038e1791ee3c35df40a56aeb508a16
Hiter Harris <hiter.harris@levvel.io>
2019-08-31 13:44:49 -0400

    added note to index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// {TODO: pass API response to Card component}
 const yelp = require('yelp-fusion');
 const apiKey = '4TwtB1xSvyHl5nDWqmOPj_3cHANyKsn8XhO2lBR2xdjRWs52PivbW-wdvQ92uWNIYR76VeQxXfSyh7jREVLe_HBd31tuPk08L5lIsHyEb449yLFbeGnPzbZGDaz_XHYx';
 const searchRequest = {
